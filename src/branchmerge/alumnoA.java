240aca5dab05b916554c3ee086b65cc7c869c345
danielmunoz-sketch <daniel.munoz@iesnervion.es>
2026-01-19 14:12:49 +0100

    segundo commit

--- src/branchmerge/alumnoA.java
+++ src/branchmerge/alumnoA.java
@@ -2,4 +2,15 @@ package branchmerge;
 
 public class alumnoA {
 
+	public static void main (String[] args) {
+		int num = 5;
+		
+		System.out.println("Buenas, esta es la clase de la rama del alumnoA (Dani)");
+		
+		if (num == 2) {
+			System.out.println("el numero es 2");
+		} else {
+			System.out.println("el numero no es 2");
+		}
+	}
 }
